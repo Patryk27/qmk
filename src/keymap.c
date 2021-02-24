7fcb76b682b1828370b9c68584d37ada00d1eabe
Patryk Wychowaniec <pwychowaniec@pm.me>
2021-02-24 17:13:56 +0100

    kbd/L1: Make KC_LCTL more handy

--- src/keymap.c
+++ src/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_PGUP, KC_HOME, KC_WH_U, KC_NO,   KC_NO, KC_NO,
         KC_NO,   KC_PGDN, KC_END,  KC_WH_D, KC_NO,   KC_NO,
         KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+        KC_LCTL, KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
 
                  KC_NO,   KC_NO,
         KC_NO,   KC_NO,   KC_NO,
