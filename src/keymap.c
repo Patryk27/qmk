c2c3f166d621c811cc77a5ae4f8d0bce4ca9e1c2
Patryk Wychowaniec <pwychowaniec@pm.me>
2021-03-23 15:03:55 +0100

    kbd: Bring back MT, reshuffle a few keys

--- src/keymap.c
+++ src/keymap.c
@@ -2,6 +2,7 @@
 
 void process_td_scln(qk_tap_dance_state_t *state, void *user_data);
 bool process_record_user_lt(uint16_t keycode, keyrecord_t *record);
+bool process_record_user_mt(uint16_t keycode, keyrecord_t *record);
 bool process_record_user_ck(uint16_t keycode, keyrecord_t *record);
 
 enum tap_dance_combos {
@@ -22,6 +23,9 @@ enum custom_keycodes {
     LT_2,
     LT_3,
 
+    MT_LCTL,
+    MT_LSFT,
+
     CK_VIM,
     CK_VIM_SL,
 
@@ -33,59 +37,59 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ergodox_80(
-        KC_MAIL, C(KC_TAB), KC_BSPC, KC_UNDS, KC_NO, KC_NO, KC_NO,
-        KC_TAB,  KC_Q,      KC_W,    KC_E,    KC_R,  KC_T,  KC_BRIU,
-        KC_ESC,  KC_A,      KC_S,    KC_D,    KC_F,  KC_G,
-        KC_LSFT, KC_Z,      KC_X,    KC_C,    KC_V,  KC_B,  KC_BRID,
-        KC_LCTL, KC_NO,     KC_NO,   LT_2,    LT_1,
+        C(KC_TAB), KC_UP,   KC_BSPC, KC_UNDS, KC_NO, KC_NO, KC_NO,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,  KC_BRIU,
+        KC_BSPC,   KC_A,    KC_S,    KC_D,    KC_F,  KC_G,
+        MT_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,  KC_BRID,
+        KC_RALT,   C(KC_C), KC_NO,   KC_DOWN, LT_1,
 
-                   KC_NO,   KC_NO,
-        CK_VIM_SL, KC_NO,   KC_NO,
-        KC_LALT,   KC_LCTL, KC_NO,
+                   KC_NO,      KC_MAIL,
+        CK_VIM_SL, KC_NO,      KC_NO,
+        LT_2,      S(KC_LCTL), RESET,
 
-        KC_NO,       KC_NO, KC_NO, KC_MINS, KC_DEL,  KC_NO,   RESET,
-        TD(TD_VOLU), KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_NO,
-                     KC_H,  KC_J,  KC_K,    KC_L,    KC_QUOT, CK_VIM,
-        TD(TD_VOLD), KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SCLN, KC_NO,
-                            LT_3,  KC_RALT, KC_NO,   KC_NO,   KC_NO,
+        KC_NO,       KC_NO, KC_SCLN, KC_MINS, KC_DEL,  KC_NO,      KC_NO,
+        TD(TD_VOLU), KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,       A(KC_X),
+                     KC_H,  KC_J,    KC_K,    KC_L,    KC_QUOT,    CK_VIM,
+        TD(TD_VOLD), KC_N,  KC_M,    KC_COMM, KC_DOT,  S(KC_SCLN), KC_NO,
+                            LT_3,    KC_NO,   KC_NO,   KC_NO,      KC_NO,
 
         CK_SAD,   CK_SMILE,
-        CK_FROWN, CK_XD,      KC_DEL,
-        KC_PSCR,  S(KC_LCTL), KC_LGUI
+        CK_FROWN, CK_XD,    KC_PSCR,
+        KC_NO,    KC_LALT,  MT_LCTL
     ),
 
     [1] = LAYOUT_ergodox_80(
         KC_NO, KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-        KC_NO, KC_SLSH,   KC_LPRN, KC_RPRN, KC_A,    KC_B,  KC_NO,
-        KC_NO, KC_BSLS,   KC_LCBR, KC_RCBR, KC_C,    KC_D,
-        KC_NO, S(KC_GRV), KC_NO,   KC_NO,   KC_E,    KC_F,  KC_NO,
+        KC_NO, KC_SLSH,   KC_LPRN, KC_RPRN, KC_NO,   KC_NO, KC_NO,
+        KC_NO, KC_BSLS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO, S(KC_GRV), KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
         KC_NO, KC_NO,     KC_NO,   KC_NO,   KC_TRNS,
 
                KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,
 
-        KC_NO, KC_NO,       KC_LT, KC_EQL, KC_GT, KC_EXLM,        KC_NO,
+        KC_NO, KC_NO,       KC_LT, KC_SPC, KC_GT, KC_EXLM,        KC_NO,
         KC_NO, KC_KP_PLUS,  KC_1,  KC_2,   KC_3,  KC_KP_ASTERISK, KC_NO,
                KC_KP_MINUS, KC_4,  KC_5,   KC_6,  KC_KP_SLASH,    KC_NO,
-        KC_NO, KC_SPC,      KC_7,  KC_8,   KC_9,  KC_COMM,        KC_NO,
+        KC_NO, KC_EQL,      KC_7,  KC_8,   KC_9,  KC_COMM,        KC_NO,
                             KC_0,  KC_DOT, KC_NO, KC_NO,          KC_NO,
 
         KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO
+        KC_A,  KC_B,  KC_C,
+        KC_D,  KC_E,  KC_F
     ),
 
     [2] = LAYOUT_ergodox_80(
-        KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5, KC_F6,
-        KC_NO,   KC_PGUP, KC_HOME, KC_WH_U, KC_NO, KC_NO, KC_NO,
-        KC_NO,   KC_PGDN, KC_END,  KC_WH_D, KC_NO, KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
-        KC_LCTL, KC_NO,   KC_NO,   KC_TRNS, KC_NO,
+        KC_NO,   KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,
+        KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
+        KC_NO,   KC_NO, KC_PGUP, KC_HOME, KC_WH_U, KC_NO,
+        KC_NO,   KC_NO, KC_PGDN, KC_END,  KC_WH_D, KC_NO, KC_NO,
+        KC_LCTL, KC_NO, KC_NO,   KC_NO,   KC_NO,
 
-               KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO,
+                 KC_NO, KC_NO,
+        KC_NO,   KC_NO, KC_NO,
+        KC_TRNS, KC_NO, KC_NO,
 
         KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_NO,
         KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,
@@ -109,9 +113,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,
 
-        KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO,   KC_DLR,  KC_NO,   KC_NO, KC_NO,
         KC_NO, KC_NO, KC_AT,   KC_LBRC, KC_RBRC, KC_NO, KC_NO,
-               KC_NO, KC_NO,   KC_DLR,  KC_NO,   KC_NO, KC_NO,
+               KC_NO, KC_NO,   KC_LCBR, KC_RCBR, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
                       KC_TRNS, KC_NO,   KC_NO,   KC_NO, KC_NO,
 
@@ -123,6 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return process_record_user_lt(keycode, record)
+        && process_record_user_mt(keycode, record)
         && process_record_user_ck(keycode, record);
 }
 
@@ -164,6 +169,53 @@ bool process_record_user_lt(uint16_t keycode, keyrecord_t *record) {
         }
 
         layer_off(layer);
+    }
+
+    return false;
+}
+
+bool process_record_user_mt(uint16_t keycode, keyrecord_t *record) {
+    static uint16_t mt_keycode = 0;
+    static bool mt_pressed = false;
+    static bool mt_interrupted = false;
+
+    uint16_t alt_keycode;
+
+    switch (keycode) {
+        case MT_LCTL:
+            mt_keycode = KC_LCTL;
+            alt_keycode = KC_LGUI;
+            break;
+
+        case MT_LSFT:
+            mt_keycode = KC_LSFT;
+            alt_keycode = KC_ESC;
+            break;
+
+        default:
+            if (mt_pressed) {
+                if (record->event.pressed) {
+                    mt_interrupted = true;
+                }
+            }
+
+            return true;
+    }
+
+    if (record->event.pressed) {
+        register_code(mt_keycode);
+
+        mt_pressed = true;
+        mt_interrupted = false;
+    } else {
+        unregister_code(mt_keycode);
+
+        if (!mt_interrupted) {
+            tap_code(alt_keycode);
+        }
+
+        mt_pressed = false;
+        mt_interrupted = false;
     }
 
     return false;
