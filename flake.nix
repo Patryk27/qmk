223ba0305cdef37b51ac107097a25e7e037638ca
Patryk Wychowaniec <pwychowaniec@pm.me>
2021-04-30 08:41:31 +0200

    refactor: Support `nix build`

--- flake.nix
+++ flake.nix
@@ -1,99 +1,66 @@
-# Based on;
+# Based on:
 # https://github.com/qmk/qmk_firmware/blob/611cd80861b1bd8ad57ef5b78f11a349e74b1949/shell.nix
 
 {
   inputs = {
     qmk = {
-      url = github:qmk/qmk_firmware;
+      type = "git";
+      url = "https://github.com/qmk/qmk_firmware.git";
+      submodules = true;
       flake = false;
     };
 
     nixpkgs = {
       url = github:nixos/nixpkgs;
     };
-  };
 
-  outputs = { self, qmk, nixpkgs }: {
-    devShell = {
-      x86_64-linux =
-        let
-          pkgs = (import nixpkgs) {
-            system = "x86_64-linux";
-          };
-
-          pkgs-avr = pkgs.pkgsCross.avr;
-
-          avrlibc = pkgs-avr.libcCross;
-
-          avr-includes = [
-            "-isystem ${avrlibc}/avr/include"
-            "-B${avrlibc}/avr/lib/avr5"
-            "-L${avrlibc}/avr/lib/avr5"
-            "-B${avrlibc}/avr/lib/avr35"
-            "-L${avrlibc}/avr/lib/avr35"
-            "-B${avrlibc}/avr/lib/avr51"
-            "-L${avrlibc}/avr/lib/avr51"
-          ];
-
-          qmk-cli = pkgs.writeShellScriptBin "qmk" ''
-            set -e
-
-            if [[ ! -d .qmk ]]; then
-              git clone \
-                --depth 1 \
-                --recurse-submodules \
-                https://github.com/qmk/qmk_firmware \
-                .qmk
-            fi
-
-            export GIT_DIR=.qmk/.git
-
-            if [[ $(git rev-parse HEAD) != "${qmk.rev}" ]]; then
-              git fetch
-              git checkout "${qmk.rev}"
-              git submodule update --recursive
-            fi
-
-            ln -s "$(pwd)/src" ./.qmk/keyboards/ergodox_ez/keymaps/custom
-            trap 'rm ./.qmk/keyboards/ergodox_ez/keymaps/custom' EXIT
-
-            ./.qmk/bin/qmk "$@"
-          '';
-
-          qmk-cli-env = pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = qmk + "/nix";
-          };
-
-          do-compile = pkgs.writeShellScriptBin "do-compile" ''
-            ${qmk-cli}/bin/qmk compile -kb ergodox_ez -km custom
-          '';
-
-          do-flash = pkgs.writeShellScriptBin "do-flash" ''
-            sudo ${qmk-cli}/bin/qmk flash -kb ergodox_ez -km custom
-          '';
-
-        in
-        pkgs.mkShell {
-          buildInputs = [
-            do-compile
-            do-flash
-
-            avrlibc
-            qmk-cli
-            qmk-cli-env
-
-            pkgs.avrdude
-            pkgs.dfu-programmer
-            pkgs.dfu-util
-            pkgs.teensy-loader-cli
-
-            pkgs-avr.buildPackages.binutils
-            pkgs-avr.buildPackages.gcc8
-          ];
-
-          AVR_CFLAGS = avr-includes;
-          AVR_ASFLAGS = avr-includes;
-        };
+    utils = {
+      url = github:numtide/flake-utils;
     };
   };
+
+  outputs = { self, qmk, nixpkgs, utils }:
+
+    utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        pkgs-avr = pkgs.pkgsCross.avr;
+
+        qmk-env = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = qmk + "/nix";
+        };
+
+        hex = pkgs.runCommandLocal "hex"
+          {
+            nativeBuildInputs = [
+              pkgs-avr.buildPackages.binutils
+              pkgs-avr.buildPackages.gcc8
+              pkgs-avr.libcCross
+            ];
+          } ''
+          cp -ar ${qmk}/* /tmp
+          chmod -R 777 /tmp/*
+          sed -i '1c#!${qmk-env}/bin/python3' /tmp/bin/qmk
+
+          ln -s ${./src} /tmp/keyboards/ergodox_ez/keymaps/custom
+          /tmp/bin/qmk compile -kb ergodox_ez -km custom
+
+          mkdir $out
+          mv /tmp/ergodox_ez_custom.hex $out/firmware.hex
+        '';
+
+      in
+      {
+        defaultPackage =
+          pkgs.writeShellScriptBin "flash" ''
+            sudo ${pkgs.teensy-loader-cli}/bin/teensy-loader-cli \
+                --mcu=atmega32u4 \
+                -w ${hex}/firmware.hex \
+                -v
+          '';
+      }
+    );
 }
